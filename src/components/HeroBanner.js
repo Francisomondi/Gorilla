5b1534d05873214c6702aff7e8c8630f77f38d30
francis omondi <francisomondi17@gmail.com>
2023-05-12 09:28:34 +0300

    home component well done..

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import { Box,Stack,Typography ,Button} from "@mui/material";
+import HeroBannerImage from '../assets/images/banner.png'
 
 const HeroBanner = () => {
   return (
-    <div>
+    <Box sx={{
+        mt:{lg:'212px',xs: '70px'},
+        ml:{sm:'50px'}
+    }} position='relative' p='20px'>
+    <Typography color='#592617' fontWeight='600' fontSize='28px'>Fitness Club</Typography>
+
+    <Typography fontWeight={700}
+    sx={{fontSize: {lg:'44px',xs:'40px'}}} mb='20px' >
+    Sweat, Smile <br/> And Gain</Typography>
+    <Typography fontSize='22px' lineHeight='35px' mb={3}>Check the most Effective workout routines</Typography>
+    <Button variant='contained' color='success' href='#exercises'>Explore Exercises</Button>
+
+    <Typography fontWeight={300}
+    color='#ff2625'
+    sx={{
+      opacity:0.5,
+      display:{lg:'block',xs:'none'}
+    }}
+    fontSize={50}
+    >Train Hard! or go home</Typography>
+    <img src={HeroBannerImage} alt='banner' className='hero-banner-img' />
       
-    </div>
+    </Box>
   )
 }
 
